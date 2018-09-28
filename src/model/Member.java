bdee0391c8bbc5a1bf9611fbcb5bc40c387d5c16
be222bw <be222bw@student.lnu.se>
2018-09-28 13:28:53 +0200

    Moving exception from model to view.

--- src/model/Member.java
+++ src/model/Member.java
@@ -12,16 +12,6 @@ public class Member {
 		name = n;
 		numOfBoats = b;
 		id = UUID.randomUUID();
-		
-		try {
-			if (!model.Verification.isCorrect(p)) {
-				throw new Exception("The personal number is not correct!");
-			}
-		} catch (Exception e) {
-			System.err.println(e.getMessage() + " Exiting.");
-			System.exit(-1);
-		}
-		
 		personalNumber = p;
 	}
 	
--- src/view/Console.java
+++ src/view/Console.java
@@ -4,7 +4,7 @@ public class Console {
 
 	/**
 	 * Identifies the first argument, and calls the relative method.
-	 * @param args Array of arguments, whose 
+	 * @param args Array of arguments, whose first element is the argument to be identified.
 	 */
 	public void identifyArgument(String[] args) {
 		switch (args[0]) {
@@ -14,9 +14,12 @@ public class Console {
 			showHelp(args);
 			break;
 		case "/cm":
+		case "--create-member":
+		case "-cm":
 			createMember(args);
 			break;
 		default:
+			System.out.println("Could not identify argument \"" + args[0] + "\"");
 			break;
 		}
 	}
@@ -35,10 +38,19 @@ public class Console {
 			System.err.println(e.getMessage());
 		}
 		model.Member member = new model.Member(args[1], args[2], Integer.parseInt(args[3]));
+		try {
+			if (!model.Verification.isCorrect(member.getPersonalNum())) {
+				throw new Exception("The personal number is not correct!");
+			}
+		} catch (Exception e) {
+			System.err.println(e.getMessage() + " Exiting.");
+			System.exit(-1);
+		}
+		
 		
 		System.out.println("Name: " + member.getName() +
 				"\nPersonal number: " + member.getPersonalNum() + "\nNumber of boats: " +
-				member.getNumOfBoats());
+				member.getNumOfBoats() + "\nId: " + member.getIdString());
 	}
 	
 
