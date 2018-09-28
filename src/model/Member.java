1d60f371ef4d1e77c99ff010d4508b778fdd7bf1
be222bw <be222bw@student.lnu.se>
2018-09-28 13:18:41 +0200

    Creating a UUID for Member.

--- src/model/Member.java
+++ src/model/Member.java
@@ -1,20 +1,25 @@
 package model;
 
+import java.util.UUID;
+
 public class Member {
 	private String name;
 	private int numOfBoats;
 	private String personalNumber;
+	private UUID id;
 	
 	public Member(String n, String p, int b) {
 		name = n;
 		numOfBoats = b;
+		id = UUID.randomUUID();
 		
 		try {
 			if (!model.Verification.isCorrect(p)) {
-				throw new Exception("The personal number is not living up to our expectations!");
+				throw new Exception("The personal number is not correct!");
 			}
 		} catch (Exception e) {
-			System.err.println(e.getMessage());
+			System.err.println(e.getMessage() + " Exiting.");
+			System.exit(-1);
 		}
 		
 		personalNumber = p;
@@ -26,6 +31,10 @@ public class Member {
 	
 	public String getPersonalNum() {
 		return personalNumber;
+	}
+	
+	public String getIdString() {
+		return id.toString();
 	}
 	
 	public int getNumOfBoats() {
--- src/view/Console.java
+++ src/view/Console.java
@@ -1,6 +1,4 @@
-package view;
-
-import java.util.Scanner;
+package view;;
 
 public class Console {
 
